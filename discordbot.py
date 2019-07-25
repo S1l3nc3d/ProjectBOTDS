5640ea71b3d06b28a156182f61f3fd56bcccc5ad
S1l3nc3d <52808905+S1l3nc3d@users.noreply.github.com>
2019-07-25 14:25:58 +0700

    Add files via upload

--- discordbot.py
+++ discordbot.py
@@ -13,7 +13,7 @@ discord.opus.load_opus
 client = commands.Bot(command_prefix='.')
 client.remove_command('help')
 
-#token = 'NjAwMTUzMzE5NjEyMDg4Mzgw.XS6VMg.3gXzU6td4ecizeDqHdsjMsh62bM'
+token = token
 
 #loggingsetup
 logger = logging.getLogger('discord')
@@ -114,4 +114,4 @@ async def music_play(ctx):
                 break
     await ctx.send("Музыка закончилась")
 
-client.run('NjAwMTUzMzE5NjEyMDg4Mzgw.XTlYuA.UhlGFjKkyrMFK8fG-uS1yvZTAfE')
+client.run(token)
